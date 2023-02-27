c53058885a5c66bf7b905fdd0be782ff01d61903
jakvusk <125307009+jakvusk@users.noreply.github.com>
2023-02-27 11:14:43 -0500

    added protag character and some animation - need to work with group to continue

---
+++ Assets/Scripts/Player/Janis Scripts/PlayerAttackSprite.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackSprite : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //checks if you can attack yet
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                
+                // makes the attack time lower so you can attack again after enough time passes
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //play attack animation
+        animator.SetTrigger("Attack");
+
+        //Detect if enemy is in range to be hit
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damge Enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+
+    }
+
+    //shows the attack sphere for player in unity
+    void OnDrawGizmosSelected() 
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/Player/Janis Scripts/PlayerMovementSprite.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementSprite : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    private enum State {
+        Normal,
+        Rolling,
+    }
+
+    private Vector3 moveDir;
+    private Vector3 lastMoveDir;
+    private Vector3 rollDir;
+    private float rollSpeed;
+    private bool rollTrue = false;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    private State state;
+
+    private void Awake() {
+        state = State.Normal;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state){
+            case State.Normal:
+        
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        moveDir = new Vector3(movement.x, movement.y).normalized;
+
+        //checks if movement is not equal to 0 (so if you are moving, the lastMoveDir is set to moveDir)
+        if (movement.x != 0 || movement.y != 0) {
+            lastMoveDir = moveDir;
+        }
+
+        //Gets the space button
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //makes the roll direction equal to your last moving direction
+            rollDir = lastMoveDir;
+            //changes how fast rolling is
+            rollSpeed = 40f;
+
+            state = State.Rolling;
+            
+        }
+            break;
+        case State.Rolling:
+            float rollSpeedDropMultiplier = 5f;
+            rollTrue = true;
+            //plays rolling animation when rolling
+            animator.SetBool("Rolling", rollTrue);
+            rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
+
+            float rollSpeedMinimum = 15f;
+            if(rollSpeed < rollSpeedMinimum){
+                state = State.Normal;
+                rollTrue = false;
+                //plays rolling animation when rolling
+                animator.SetBool("Rolling", rollTrue);
+            }
+            break;
+        }
+        
+    }
+
+    //movement
+    void FixedUpdate()
+    {
+        switch (state) {
+            case State.Normal:
+                //Normal WASD movement
+                // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+                rb.velocity = moveDir * moveSpeed;
+
+        
+        break;
+        //rolling movement.
+    case State.Rolling:
+        rb.velocity = rollDir * rollSpeed;
+        break;
+        }
+    }
+}
