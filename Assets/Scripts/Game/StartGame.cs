669db4dd48f4e8911ecd66f855ea8a2bca096037
devant6 <devant6@hotmail.com>
2023-03-20 21:02:02 -0400

    click menu

--- Assets/Scripts/Game/StartGame.cs
+++ Assets/Scripts/Game/StartGame.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    public AudioSource clickAudio;
     //starts the game on button click 
     public void PlayGame()
     {
+        clickAudio.Play();
         SceneManager.LoadScene(1);
     }
     public void QuitGame() 
     {
+        clickAudio.Play();
         Application.Quit();
     }
 
